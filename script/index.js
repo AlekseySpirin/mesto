b22e03d084d357416ae5314d1e34ee81b0670963
Aleksey Spirin <spirin.mezhmun@yandex.ru>
2023-01-12 18:09:19 +0300

    попытки сохранения формы

--- script/index.js
+++ script/index.js
@@ -15,8 +15,31 @@ popUpClose.addEventListener("click", function () {
 // submit
 
 let formElement = document.querySelector(".pop-up");
+let nameInput = formElement.querySelector(".pop-up__name");
+let jobInput = formElement.querySelector(".pop-up__job");
 
-let nameInput = formElement.querySelector("");
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let profileName = document.querySelector(".profile__name");
+  let profileInfo = document.querySelector(".profile__info");
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
+//------------------------------------------------------------
+
+// button-save
+
+// let buttonSave = document.querySelector(".pop-up__button-save");
+
+// buttonSave.addEventListener("click", function () {
+//   popUp.classList.remove("pop-up_active");
+// });
+
+//------------------------------------------------------
+// сделать чтобы информация не сохранялась при нажатии на крестик
+// запретить применение функции при нажатии на close
 
 // like
 
